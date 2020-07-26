e9909467f198830db7542b3f4b6e6cd6326509a9
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-07-26 18:35:03 +0200

    import the component Footer.vue in src/App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <Header />
+    <Footer />
     <router-view />
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default {
 
   components: {
-    Header
+    Header,
+    Footer
   },
 
   data: () => ({
