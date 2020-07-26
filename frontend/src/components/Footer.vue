ad49af105e702182da82ae7dc304bedeb40112e7
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-07-26 18:34:43 +0200

    add the component Footer.vue

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="footer">
+        <p> Ce site est réalisé dans le cadre d'un exercice perso</p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+#footer{
+    position: absolute;
+    bottom: 0;
+    left: 40%
+}
+</style>
