02cd36956bfe697d3bad4079c5f3c8ede7818d7f
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-07-27 21:08:47 +0200

    add content in the component contentIndex.vue

--- frontend/src/components/contentIndex.vue
+++ frontend/src/components/contentIndex.vue
@@ -1,6 +1,28 @@
 <template>
     <div>
-        
-    </div>
+        <v-col cols="12">
+            <v-container style="text-align:center">       
+                <v-sheet :width="1200">
+                    <h1>The solar system</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, fugit tempora velit animi natus quas nemo laborum facilis dicta est provident corrupti inventore iure neque consequuntur possimus accusamus. Non, suscipit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nisi voluptatem necessitatibus, esse inventore voluptate odio numquam animi consectetur a deleniti qui consequatur ipsum aliquam vero quidem ex vel similique.
+                        </p>
+
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, fugit tempora velit animi natus quas nemo laborum facilis dicta est provident corrupti inventore iure neque consequuntur possimus accusamus. Non, suscipit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nisi voluptatem necessitatibus, esse inventore voluptate odio numquam animi consectetur a deleniti qui consequatur ipsum aliquam vero quidem ex vel similique.
+                        </p>
+
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, fugit tempora velit animi natus quas nemo laborum facilis dicta est provident corrupti inventore iure neque consequuntur possimus accusamus. Non, suscipit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nisi voluptatem necessitatibus, esse inventore voluptate odio numquam animi consectetur a deleniti qui consequatur ipsum aliquam vero quidem ex vel similique.
+                        </p>    
+                </v-sheet>       
+            </v-container>
+        </v-col>
+  </div>
 </template>
 
+<style>
+body{
+    background-color: rgb(34, 32, 32)
+}
+</style>
