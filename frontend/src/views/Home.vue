aa503f97ca999a79160579d22f4d07a13ee326e2
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-07-27 15:22:09 +0200

    import the component contentIndex in views/Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="home">
-
+    <contentIndex/>
   </div>
 </template>
 
 <script>
+import contentIndex from '../components/contentIndex'
 // @ is an alias to /src
 
 export default {
   name: "Home",
   components: {
+    contentIndex
   }
 };
 </script>
