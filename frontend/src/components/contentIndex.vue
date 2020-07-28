a424558ce125f089607ef2b3663c4fa53ed3be4c
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-07-28 19:47:47 +0200

    add a background-color to the body, and a class (content) with a background-color and color:white in components/contentIndex.vue

--- frontend/src/components/contentIndex.vue
+++ frontend/src/components/contentIndex.vue
@@ -2,17 +2,17 @@
     <div>
         <v-col cols="12">
             <v-container style="text-align:center">       
-                <v-sheet :width="1200">
-                    <h1>The solar system</h1>
-                        <p>
+                <v-sheet :width="1145">
+                    <h1 class="content">The solar system</h1>
+                        <p class="content">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, fugit tempora velit animi natus quas nemo laborum facilis dicta est provident corrupti inventore iure neque consequuntur possimus accusamus. Non, suscipit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nisi voluptatem necessitatibus, esse inventore voluptate odio numquam animi consectetur a deleniti qui consequatur ipsum aliquam vero quidem ex vel similique. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit inventore perspiciatis quos repudiandae molestias atque sint in, provident, asperiores molestiae maiores aliquam iusto eaque fuga. Labore sed ab cupiditate magni!
+                        </p>
+
+                        <p class="content">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, fugit tempora velit animi natus quas nemo laborum facilis dicta est provident corrupti inventore iure neque consequuntur possimus accusamus. Non, suscipit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nisi voluptatem necessitatibus, esse inventore voluptate odio numquam animi consectetur a deleniti qui consequatur ipsum aliquam vero quidem ex vel similique.
                         </p>
 
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, fugit tempora velit animi natus quas nemo laborum facilis dicta est provident corrupti inventore iure neque consequuntur possimus accusamus. Non, suscipit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nisi voluptatem necessitatibus, esse inventore voluptate odio numquam animi consectetur a deleniti qui consequatur ipsum aliquam vero quidem ex vel similique.
-                        </p>
-
-                        <p>
+                        <p class="content"> 
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, fugit tempora velit animi natus quas nemo laborum facilis dicta est provident corrupti inventore iure neque consequuntur possimus accusamus. Non, suscipit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nisi voluptatem necessitatibus, esse inventore voluptate odio numquam animi consectetur a deleniti qui consequatur ipsum aliquam vero quidem ex vel similique.
                         </p>    
                 </v-sheet>       
@@ -23,6 +23,11 @@
 
 <style>
 body{
-    background-color: rgb(34, 32, 32)
+    background-color: black
+}
+
+.content{
+    background-color: rgb(34, 32, 32);
+    color:white
 }
 </style>
