35a5f9e77fbeb44feea6dd4cc889c31acadf8da1
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-07-30 23:45:56 +0200

    change the height for v-bottom-navigation, and change the background-color of every button in the component Header.vue

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -2,41 +2,41 @@
     <div style="text-align:center">
       <h1 class="content"><router-link to="/" style="color:white;text-decoration:none">Solar System</router-link></h1>
       
-       <v-bottom-navigation grow height="2.5rem">
+       <v-bottom-navigation grow height="2.24rem">
 
-    <v-btn :value="activeBtn" style="color:#f0f068" class="button">
+    <v-btn :value="activeBtn" style="background-color: rgb(34, 32, 32)" class="button">
       <strong><router-link to="/" style="color:#f0f068">Sun</router-link></strong>
     </v-btn>
 
-    <v-btn :value="activeBtn" style="color:#808080" class="button">
+    <v-btn :value="activeBtn" style="background-color: rgb(34, 32, 32)" class="button">
       <strong><router-link to="/" style="color:#808080">Mercury</router-link></strong>
     </v-btn>
 
-    <v-btn :value="activeBtn" style="color:#F4593D" class="button">
-      <strong><router-link to="/" style="color:#F4593D">Venus</router-link></strong>
+    <v-btn :value="activeBtn" style="background-color: rgb(34, 32, 32)" class="button">
+      <strong><router-link to="/">Venus</router-link></strong>
     </v-btn>
 
-    <v-btn :value="activeBtn" style="color:#72dbf2" class="button">
+    <v-btn :value="activeBtn" style="background-color: rgb(34, 32, 32)" class="button">
       <strong><router-link to="/" style="color:#72dbf2">Earth</router-link></strong>
     </v-btn>
 
-    <v-btn :value="activeBtn" style="color:#f18400" class="button">
+    <v-btn :value="activeBtn" style="background-color: rgb(34, 32, 32)" class="button">
       <strong><router-link to="/" style="color:#f18400">Mars</router-link></strong>
     </v-btn>
 
-    <v-btn :value="activeBtn" style="color:#ed7646" class="button">
+    <v-btn :value="activeBtn" style="background-color: rgb(34, 32, 32)" class="button">
       <strong><router-link to="/" style="color:#ed7646">Jupiter</router-link></strong>
     </v-btn>
 
-    <v-btn :value="activeBtn" style="color:#CAC7A7" class="button">
+    <v-btn :value="activeBtn" style="background-color: rgb(34, 32, 32)" class="button">
       <strong><router-link to="/" style="color:#CAC7A7">Saturn</router-link></strong>
     </v-btn>
 
-    <v-btn :value="activeBtn" style="color:#ADE7E9" class="button">
+    <v-btn :value="activeBtn" style="background-color: rgb(34, 32, 32)" class="button">
       <strong><router-link to="/" style="color:#ADE7E9">Uranus</router-link></strong>
     </v-btn>
 
-    <v-btn :value="activeBtn" style="color:#2ED1D3" class="button">
+    <v-btn :value="activeBtn" style="background-color: rgb(34, 32, 32)" class="button">
       <strong><router-link to="/" style="color:#ADE7E9">Neptune</router-link></strong>
     </v-btn>
 
