d53e7b0d4387db5dae7d3c0896317d4f540d85b8
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-07-31 19:21:07 +0200

    add imports as a comment for future use in router/index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,16 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-
+import Sun from "../views/Sun.vue";
+/*import Mercury from "../views/Mercury.vue";
+import Venus from "../views/Venus.vue";
+import Earth from "../views/Earth.vue";
+import Mars from "../views/Mars.vue";
+import Jupiter from "../views/Jupiter.vue";
+import Saturn from "../views/Saturn.vue";
+import Uranus from "../views/Uranus.vue";
+import Neptune from "../views/Neptune.vue";
+*/
 Vue.use(VueRouter);
 
 const routes = [
@@ -16,6 +25,58 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
   },
+  {
+    path: "/sun",
+    name: "Sun",
+    component: Sun
+  },
+  {
+    path: "/sun",
+    name: "Sun",
+    component: Sun
+  },
+/*
+  {
+    path: "/mercury",
+    name: "Mercury",
+    component: Mercury
+  },
+  {
+    path: "/venus",
+    name: "Venus",
+    component: Venus
+  },
+  {
+    path: "/earth",
+    name: "Earth",
+    component: Earth
+  },
+    {
+    path: "/mars",
+    name: "Mars",
+    component: Mars
+  },
+    {
+    path: "/jupiter",
+    name: "Jupiter",
+    component: Jupiter
+  },
+    {
+    path: "/saturne",
+    name: "Saturne",
+    component: Saturne
+  },
+    {
+    path: "/uranus",
+    name: "Uranus",
+    component: Uranus
+  },
+  {
+    path: "/neputune",
+    name: "Neptune",
+    component: Neptune
+  },
+*/
   {
     path: "*",
     redirect: '/'
