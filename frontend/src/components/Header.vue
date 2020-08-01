22d7d206c3c7f8e609d7b33fc726f4a7e266be1b
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-01 14:50:34 +0200

    add the real path for each button in the component Header.vue

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -9,35 +9,35 @@
     </v-btn>
 
     <v-btn :value="activeBtn" style="background-color: rgb(34, 32, 32)" class="button">
-      <strong><router-link to="/" style="color:#808080">Mercury</router-link></strong>
+      <strong><router-link to="/mercury" style="color:#808080">Mercury</router-link></strong>
     </v-btn>
 
     <v-btn :value="activeBtn" style="background-color: rgb(34, 32, 32)" class="button">
-      <strong><router-link to="/">Venus</router-link></strong>
+      <strong><router-link to="/venus" style="color:#F4593D">Venus</router-link></strong>
     </v-btn>
 
     <v-btn :value="activeBtn" style="background-color: rgb(34, 32, 32)" class="button">
-      <strong><router-link to="/" style="color:#72dbf2">Earth</router-link></strong>
+      <strong><router-link to="/earth" style="color:#72dbf2">Earth</router-link></strong>
     </v-btn>
 
     <v-btn :value="activeBtn" style="background-color: rgb(34, 32, 32)" class="button">
-      <strong><router-link to="/" style="color:#f18400">Mars</router-link></strong>
+      <strong><router-link to="/mars" style="color:#f18400">Mars</router-link></strong>
     </v-btn>
 
     <v-btn :value="activeBtn" style="background-color: rgb(34, 32, 32)" class="button">
-      <strong><router-link to="/" style="color:#ed7646">Jupiter</router-link></strong>
+      <strong><router-link to="/jupiter" style="color:#ed7646">Jupiter</router-link></strong>
     </v-btn>
 
     <v-btn :value="activeBtn" style="background-color: rgb(34, 32, 32)" class="button">
-      <strong><router-link to="/" style="color:#CAC7A7">Saturn</router-link></strong>
+      <strong><router-link to="/saturn" style="color:#CAC7A7">Saturn</router-link></strong>
     </v-btn>
 
     <v-btn :value="activeBtn" style="background-color: rgb(34, 32, 32)" class="button">
-      <strong><router-link to="/" style="color:#ADE7E9">Uranus</router-link></strong>
+      <strong><router-link to="/uranus" style="color:#ADE7E9">Uranus</router-link></strong>
     </v-btn>
 
     <v-btn :value="activeBtn" style="background-color: rgb(34, 32, 32)" class="button">
-      <strong><router-link to="/" style="color:#ADE7E9">Neptune</router-link></strong>
+      <strong><router-link to="/neptune" style="color:#ADE7E9">Neptune</router-link></strong>
     </v-btn>
 
   </v-bottom-navigation>
