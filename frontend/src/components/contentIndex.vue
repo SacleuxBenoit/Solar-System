37d1505dd736dd5c1116a4996e7853520ed73409
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-01 18:43:22 +0200

    delete useless code in the component contentIndex.vue

--- frontend/src/components/contentIndex.vue
+++ frontend/src/components/contentIndex.vue
@@ -4,16 +4,16 @@
             <v-container style="text-align:center">       
                 <v-sheet :width="1145">
                     <v-col class="content border">
-                    <h1 class="content">The solar system</h1>
-                        <p class="content">
+                    <h1>The solar system</h1>
+                        <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, fugit tempora velit animi natus quas nemo laborum facilis dicta est provident corrupti inventore iure neque consequuntur possimus accusamus. Non, suscipit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nisi voluptatem necessitatibus, esse inventore voluptate odio numquam animi consectetur a deleniti qui consequatur ipsum aliquam vero quidem ex vel similique. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit inventore perspiciatis quos repudiandae molestias atque sint in, provident, asperiores molestiae maiores aliquam iusto eaque fuga. Labore sed ab cupiditate magni!
                         </p>
 
-                        <p class="content">
+                        <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, fugit tempora velit animi natus quas nemo laborum facilis dicta est provident corrupti inventore iure neque consequuntur possimus accusamus. Non, suscipit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nisi voluptatem necessitatibus, esse inventore voluptate odio numquam animi consectetur a deleniti qui consequatur ipsum aliquam vero quidem ex vel similique.
                         </p>
 
-                        <p class="content"> 
+                        <p> 
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, fugit tempora velit animi natus quas nemo laborum facilis dicta est provident corrupti inventore iure neque consequuntur possimus accusamus. Non, suscipit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nisi voluptatem necessitatibus, esse inventore voluptate odio numquam animi consectetur a deleniti qui consequatur ipsum aliquam vero quidem ex vel similique. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores blanditiis, officiis temporibus commodi modi nisi ducimus maxime in eius sunt voluptates omnis iste porro debitis quod error possimus cumque. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam sunt distinctio perspiciatis veniam enim eum, nulla, recusandae quos est architecto harum sed aperiam assumenda voluptatum, ea praesentium! Laboriosam, odit ipsam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium odio sed a doloremque provident voluptates fuga natus, ratione optio sequi omnis explicabo enim possimus eum delectus quod fugit adipisci nulla? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium at ut eaque maxime voluptatibus repellendus sequi inventore ab, id velit mollitia architecto odit temporibus error maiores nemo corporis consequuntur dignissimos.
                         </p>  
                         </v-col>  
