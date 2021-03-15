b1bc76225a05764c732394b6c7a8dbce36d7f5da
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-15 15:23:12 +0100

    create the folder components, add header.php in it with h1, and a list with planets

---
+++ components/Header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Solar System</h1>
+
+    <ul>
+        <li><a href="#">Sun</a></li>
+        <li><a href="#">Mercury</a></li>
+        <li><a href="#">Venus</a></li>
+        <li><a href="#">Earth</a></li>
+        <li><a href="#">Mars</a></li>
+        <li><a href="#">Jupiter</a></li>
+        <li><a href="#">Saturn</a></li>
+        <li><a href="#">Uranus</a></li>
+        <li><a href="#">Neptune</a></li>
+    </ul>
+</body>
+</html>
